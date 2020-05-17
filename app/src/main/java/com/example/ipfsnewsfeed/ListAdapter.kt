09c2b31429b818710805a49b803b46dc5eaad20b
Mr. Motanovici <44145743+Motanovici@users.noreply.github.com>
2020-05-17 16:18:20 +0300

    Update ListAdapter.kt

--- app/src/main/java/com/example/ipfsnewsfeed/ListAdapter.kt
+++ app/src/main/java/com/example/ipfsnewsfeed/ListAdapter.kt
@@ -4,6 +4,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
+import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.news_row.view.*
 
 
@@ -11,11 +12,11 @@ import kotlinx.android.synthetic.main.news_row.view.*
 class ListAdapter(val homeFeed: HomeFeed) : RecyclerView.Adapter<CustomViewHolder>() {
 
 
-    //note: okhttp version 14 throws error , keep 12
+    //note: okhttp version 14 throws error , keep 12 until the code is adapted for the new version
 
 
     override fun getItemCount(): Int {
-        return 10 //homeFeed.pairs.count()
+        return homeFeed.data.count()
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
@@ -28,11 +29,13 @@ class ListAdapter(val homeFeed: HomeFeed) : RecyclerView.Adapter<CustomViewHolde
     override fun onBindViewHolder(holder: CustomViewHolder, position: Int) {
 
         val newsTitle = homeFeed.data.get(position)
-        holder?.view?.textView_newsTitle?.text = newsTitle.price_usd.toString()
-
+        holder?.view?.textView_newsTitle?.text = "Price in $: " + newsTitle.price_usd.toString()
         holder?.view?.textView_cont?.text = newsTitle.name
+        holder?.view?.textView_tsup?.text = "Circulating supply: " + newsTitle.tsupply.toString() + "$"
+        holder?.view?.textView_mkcap?.text = "Market cap: " + newsTitle.market_cap_usd.toString() + "$"
 
-
+        val cryptoThumb = holder?.view?.imageView
+        Picasso.get().load("https://mrcryptodude.com/wp-content/uploads/2018/10/bitcoin-storage-thumbnail-1080x675.png").into(cryptoThumb)
 
 
     }
